b7a4d061521e3816b9ef4c3f30f6a30a964a8c44
redips <redips@prismweaver.com>
2022-12-04 18:44:26 +0900

    [amuna] first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('prod', 'dev').default('dev'),
+        PORT: Joi.number().default(4100),
+      }),
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
